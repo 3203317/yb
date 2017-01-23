6380af9c4a5634f43c8da00d6ca6e970fe578583
unknown <3203317@qq.com>
2017-01-23 23:52:43 +0800

    haha

--- original_java/src/test/java/net/foreworld/test/RoleServiceTest.java
+++ original_java/src/test/java/net/foreworld/test/RoleServiceTest.java
@@ -5,9 +5,11 @@ import java.util.List;
 import javax.annotation.Resource;
 import javax.transaction.Transactional;
 
+import net.foreworld.model.ResultMap;
 import net.foreworld.model.Role;
 import net.foreworld.service.RoleService;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.test.annotation.Rollback;
 
@@ -19,10 +21,22 @@ public class RoleServiceTest extends BasicTest {
 	@Test
 	@Transactional
 	@Rollback(false)
-	// 标明使用完此方法后事务不回滚,true时为回滚
+	// 标明使用完此方法后事务不回滚, true时为回滚
 	public void test_findByRole() {
-
 		List<Role> list = roleService.findByRole(null, 1, Integer.MAX_VALUE);
-		System.out.println(list.size());
+		System.out.println("-----" + list.size());
 	}
+
+	@Test
+	public void test_saveNew() {
+		Role entity = new Role();
+		entity.setRole_name("role_name");
+		entity.setRole_desc("role_desc");
+		entity.setStatus(RoleService.Status.START.value());
+
+		ResultMap<Role> map = roleService.saveNew(entity);
+
+		Assert.assertTrue(map.getMsg(), map.getSuccess());
+	}
+
 }
