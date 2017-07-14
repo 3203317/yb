cd183f6022772c2f0158b07f472475a86f470508
huangxin <3203317@qq.com>
2017-07-14 15:34:05 +0800

    haha

---
+++ abc-app/src/test/java/net/abc/test/SysCfgServiceTest.java
@@ -0,0 +1,37 @@
+package net.abc.test;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import net.abc.model.ResultMap;
+import net.abc.model.SysCfg;
+import net.abc.service.SysCfgService;
+
+public class SysCfgServiceTest extends BasicTest {
+
+	@Resource
+	private SysCfgService sysCfgService;
+
+	@Test
+	public void test_findBySysCfg() {
+		List<SysCfg> list = sysCfgService.findBySysCfg(null, 1, Integer.MAX_VALUE);
+		System.out.println("-----" + list.size());
+	}
+
+	@Test
+	public void test_editInfo() {
+		SysCfg entity = new SysCfg();
+		entity.setKey_("01");
+		entity.setValue_("02");
+		entity.setTitle("03");
+		entity.setComment("04");
+
+		ResultMap<Void> map = sysCfgService.editInfo(entity);
+		Assert.assertTrue(map.getMsg(), map.getSuccess());
+	}
+
+}
