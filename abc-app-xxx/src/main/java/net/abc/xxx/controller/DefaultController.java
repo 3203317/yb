a017406009ba14c20ecd2f9d798798c275614230
huangxin <3203317@qq.com>
2017-07-19 17:53:35 +0800

    haha

--- abc-app-xxx/src/main/java/net/abc/xxx/controller/DefaultController.java
+++ abc-app-xxx/src/main/java/net/abc/xxx/controller/DefaultController.java
@@ -11,36 +11,35 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import net.abc.controller.BaseController;
 
 /**
- *
- * @author Administrator
+ * 
+ * @author huangxin <3203317@qq.com>
  *
  */
 @Controller
 public class DefaultController extends BaseController {
 
-	/**
-	 * 用户首页
-	 *
-	 * @param session
-	 * @param map
-	 * @return
-	 */
-	@RequestMapping(value = { "", "/" }, method = RequestMethod.GET)
-	public String _i_indexUI(HttpSession session, Map<String, Object> map) {
-		map.put("session_user", session.getAttribute("session.user"));
-		map.put("nav_choose", ",01,");
-		return "i/default/1.0.2/index";
-	}
+	// /**
+	// * 用户首页
+	// *
+	// * @param session
+	// * @param map
+	// * @return
+	// */
+	// @RequestMapping(value = { "", "/" }, method = RequestMethod.GET)
+	// public String _i_indexUI(HttpSession session, Map<String, Object> map) {
+	// map.put("session_user", session.getAttribute("session.user"));
+	// map.put("nav_choose", ",01,");
+	// return "i/default/1.0.2/index";
+	// }
 
 	/**
-	 * 后台首页
-	 *
+	 * 
 	 * @param session
 	 * @param map
 	 * @return
 	 */
-	@RequestMapping(value = { "/manage/" }, method = RequestMethod.GET)
-	public String _m_indexUI(HttpSession session, Map<String, Object> map) {
+	@RequestMapping(value = { "/" }, method = RequestMethod.GET)
+	public String indexUI(HttpSession session, Map<String, Object> map) {
 		map.put("session_user", session.getAttribute("session.user"));
 		map.put("nav_choose", ",01,");
 		return "m/default/index";
