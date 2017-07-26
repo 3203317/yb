df1c17080ec18b660d014323bd7270e9068796fe
huangxin <3203317@qq.com>
2017-07-26 16:46:18 +0800

    haha

--- abc-biz-xxx-impl/src/main/java/net/abc/xxx/service/impl/ProjEntityPropServiceImpl.java
+++ abc-biz-xxx-impl/src/main/java/net/abc/xxx/service/impl/ProjEntityPropServiceImpl.java
@@ -1,1 +1,61 @@
-package net.abc.xxx.service.impl;import java.util.List;import org.springframework.stereotype.Service;import net.abc.model.ResultMap;import net.abc.service.impl.BaseService;import net.abc.xxx.model.ProjEntityProp;import net.abc.xxx.service.ProjEntityPropService;/** *  * @author huangxin <3203317@qq.com> * */@Service("projEntityPropService")public class ProjEntityPropServiceImpl extends BaseService<ProjEntityProp> implements ProjEntityPropService {	@Override	public List<ProjEntityProp> findByProjEntityProp(ProjEntityProp entity, int page, int rows) {		// TODO Auto-generated method stub		return null;	}	@Override	public ResultMap<Void> editInfo(ProjEntityProp entity) {		// TODO Auto-generated method stub		return null;	}	@Override	public ResultMap<ProjEntityProp> saveNew(ProjEntityProp entity) {		// TODO Auto-generated method stub		return null;	}}
+package net.abc.xxx.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.github.pagehelper.PageHelper;
+
+import net.abc.model.ResultMap;
+import net.abc.service.impl.BaseService;
+import net.abc.util.StringUtil;
+import net.abc.xxx.model.Proj;
+import net.abc.xxx.model.ProjEntityProp;
+import net.abc.xxx.service.ProjEntityPropService;
+import tk.mybatis.mapper.entity.Example;
+
+/**
+ * 
+ * @author huangxin <3203317@qq.com>
+ *
+ */
+@Service("projEntityPropService")
+public class ProjEntityPropServiceImpl extends BaseService<ProjEntityProp> implements ProjEntityPropService {
+
+	@Override
+	public List<ProjEntityProp> findByProjEntityProp(ProjEntityProp entity, int page, int rows) {
+
+		Example example = new Example(Proj.class);
+		example.setOrderByClause("create_time desc");
+
+		if (null != entity) {
+			Example.Criteria criteria = example.createCriteria();
+
+			String proj_id = StringUtil.isEmpty(entity.getProj_id());
+			if (null != proj_id) {
+				criteria.andEqualTo("proj_id", proj_id);
+			}
+
+			String entity_id = StringUtil.isEmpty(entity.getEntity_id());
+			if (null != entity_id) {
+				criteria.andEqualTo("entity_id", entity_id);
+			}
+		}
+
+		PageHelper.startPage(page, rows);
+		return selectByExample(example);
+	}
+
+	@Override
+	public ResultMap<Void> editInfo(ProjEntityProp entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResultMap<ProjEntityProp> saveNew(ProjEntityProp entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- abc-biz-xxx-impl/src/main/java/net/abc/xxx/service/impl/ProjEntityServiceImpl.java
+++ abc-biz-xxx-impl/src/main/java/net/abc/xxx/service/impl/ProjEntityServiceImpl.java
@@ -1,1 +1,56 @@
-package net.abc.xxx.service.impl;import java.util.List;import org.springframework.stereotype.Service;import net.abc.model.ResultMap;import net.abc.service.impl.BaseService;import net.abc.xxx.model.ProjEntity;import net.abc.xxx.service.ProjEntityService;/** *  * @author huangxin <3203317@qq.com> * */@Service("projEntityService")public class ProjEntityServiceImpl extends BaseService<ProjEntity> implements ProjEntityService {	@Override	public List<ProjEntity> findByProjEntity(ProjEntity entity, int page, int rows) {		// TODO Auto-generated method stub		return null;	}	@Override	public ResultMap<Void> editInfo(ProjEntity entity) {		// TODO Auto-generated method stub		return null;	}	@Override	public ResultMap<ProjEntity> saveNew(ProjEntity entity) {		// TODO Auto-generated method stub		return null;	}}
+package net.abc.xxx.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.github.pagehelper.PageHelper;
+
+import net.abc.model.ResultMap;
+import net.abc.service.impl.BaseService;
+import net.abc.util.StringUtil;
+import net.abc.xxx.model.Proj;
+import net.abc.xxx.model.ProjEntity;
+import net.abc.xxx.service.ProjEntityService;
+import tk.mybatis.mapper.entity.Example;
+
+/**
+ * 
+ * @author huangxin <3203317@qq.com>
+ *
+ */
+@Service("projEntityService")
+public class ProjEntityServiceImpl extends BaseService<ProjEntity> implements ProjEntityService {
+
+	@Override
+	public List<ProjEntity> findByProjEntity(ProjEntity entity, int page, int rows) {
+
+		Example example = new Example(Proj.class);
+		example.setOrderByClause("create_time desc");
+
+		if (null != entity) {
+			Example.Criteria criteria = example.createCriteria();
+
+			String proj_id = StringUtil.isEmpty(entity.getProj_id());
+			if (null != proj_id) {
+				criteria.andEqualTo("proj_id", proj_id);
+			}
+		}
+
+		PageHelper.startPage(page, rows);
+		return selectByExample(example);
+	}
+
+	@Override
+	public ResultMap<Void> editInfo(ProjEntity entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResultMap<ProjEntity> saveNew(ProjEntity entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
