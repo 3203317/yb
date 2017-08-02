0b94c3a8d78431f89988efb04e3e81bee9155f28
huangxin <3203317@qq.com>
2017-08-02 15:50:48 +0800

    haha

--- abc-app-xxx/assets/template/mysql.sql
+++ abc-app-xxx/assets/template/mysql.sql
@@ -4,6 +4,6 @@
 create table ${(data_pe.db_name)!} 
 (
   <#import "data_type_mysql" as data_type>
-<#list data_list_pep! as doc>  ${(doc.prop_name)!}  <@data_type.m name="${(doc.prop_type)!}"/>  null,
+<#list data_list_pep! as doc>  ${(doc.prop_name)!}  <@data_type.m name="${(doc.prop_type)!}"/>  null<#if doc_has_next>,</#if>
 </#list>
 );
