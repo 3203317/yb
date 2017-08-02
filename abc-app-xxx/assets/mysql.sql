a2f1dceb1437e6b85c5fd1ce70d8c15404530ef1
huangxin <3203317@qq.com>
2017-08-02 15:30:18 +0800

    haha

--- abc-app-xxx/assets/mysql.sql
+++ abc-app-xxx/assets/mysql.sql
@@ -3,7 +3,7 @@
  */
 create table ${(data_pe.db_name)!} 
 (
-  <#import "data_type_java" as data_type>
+  <#import "data_type_mysql" as data_type>
 <#list data_list_pep! as doc>  ${(doc.prop_name)!}  <@data_type.m name="${(doc.prop_type)!}"/>  null,
 </#list>
 );
