cfacae34b1eb12006a147c76e24e83835fd92aa0
huangxin <3203317@qq.com>
2017-11-27 16:32:19 +0800

    haha

---
+++ abc-app-xxx/src/main/java/net/abc/xxx/controller/ProjEntityPropController.java
@@ -0,0 +1,53 @@
+package net.abc.xxx.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import net.abc.controller.BaseController;
+import net.abc.xxx.model.ProjEntityProp;
+import net.abc.xxx.service.ProjEntityPropService;
+
+/**
+ * 
+ * @author huangxin <3203317@qq.com>
+ *
+ */
+@RequestMapping(value = { "/proj/entity" })
+@Controller
+public class ProjEntityPropController extends BaseController {
+
+	@Resource
+	private ProjEntityPropService projEntityPropService;
+
+	/**
+	 * 
+	 * @param session
+	 * @param entity_id
+	 * @param map
+	 * @return
+	 */
+	@RequestMapping(value = { "/prop/" }, method = RequestMethod.GET)
+	public String indexUI(HttpSession session, @RequestParam(required = true) String entity_id,
+			Map<String, Object> map) {
+
+		map.put("session_user", session.getAttribute("session.user"));
+		map.put("nav_choose", ",05,0502,");
+
+		ProjEntityProp pep = new ProjEntityProp();
+		pep.setEntity_id(entity_id);
+
+		List<ProjEntityProp> list = projEntityPropService.findByProjEntityProp(pep, 1, Integer.MAX_VALUE);
+		map.put("data_list_pep", list);
+
+		return "proj/entity/prop/index";
+	}
+
+}
