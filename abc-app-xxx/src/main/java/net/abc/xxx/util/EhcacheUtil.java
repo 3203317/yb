c31d451ef69448f37b32da28ef3d3e754c7720ed
unknown <3203317@qq.com>
2018-01-04 23:18:14 +0800

    haha

--- abc-app-xxx/src/main/java/net/abc/xxx/util/EhcacheUtil.java
+++ abc-app-xxx/src/main/java/net/abc/xxx/util/EhcacheUtil.java
@@ -1,27 +1,30 @@
 package net.abc.xxx.util;
 
+import java.io.InputStream;
+
 import net.sf.ehcache.Cache;
 import net.sf.ehcache.CacheManager;
 import net.sf.ehcache.Element;
 
 /**
- * 
+ *
  * @author huangxin <3203317@qq.com>
  *
  */
 public class EhcacheUtil {
 
 	public static void main(String[] args) {
-		CacheManager mgr = new CacheManager(
-				"/home/huangxin/my/git/3203317/yb/abc-app-xxx/src/main/resources/ehcache.xml");
+
+		InputStream is = EhcacheUtil.class.getResourceAsStream("/ehcache.xml");
+		CacheManager mgr = CacheManager.create(is);
 		Cache cache = mgr.getCache("test_ehcache");
 		Element ele = new Element("a", "b");
 		cache.put(ele);
 
-		final Cache cache2 = mgr.getCache("test_ehcache");
-		final Element e = cache2.get("a");
+		final Element e = cache.get("a");
 
 		System.err.println(e.getObjectValue());
+		System.err.println(ele == e);
 	}
 
 }
