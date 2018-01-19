db71a3f33ccc86a81e1420cf6a0b15a6b21554ac
speedt <13837186852@qq.com>
2018-01-19 15:26:25 +0800

    haha

--- abc-app-xxx/src/main/java/net/abc/xxx/controller/ProjController.java
+++ abc-app-xxx/src/main/java/net/abc/xxx/controller/ProjController.java
@@ -7,11 +7,6 @@ import java.util.Map;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import net.abc.controller.BaseController;
 import net.abc.util.StringUtil;
 import net.abc.util.freemarker.Processor;
@@ -19,13 +14,20 @@ import net.abc.xxx.init.FreemarkerTemplateResource;
 import net.abc.xxx.model.Proj;
 import net.abc.xxx.model.ProjEntity;
 import net.abc.xxx.model.ProjEntityProp;
+import net.abc.xxx.model.ProjForm;
 import net.abc.xxx.service.ProjEntityPropService;
 import net.abc.xxx.service.ProjEntityService;
+import net.abc.xxx.service.ProjFormService;
 import net.abc.xxx.service.ProjService;
 import net.abc.xxx.util.TempUtil;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 /**
- * 
+ *
  * @author huangxin <3203317@qq.com>
  *
  */
@@ -44,8 +46,11 @@ public class ProjController extends BaseController {
 	@Resource
 	private ProjEntityPropService projEntityPropService;
 
+	@Resource
+	private ProjFormService projFormService;
+
 	/**
-	 * 
+	 *
 	 * @param session
 	 * @param lang_id
 	 * @param db_id
@@ -56,9 +61,12 @@ public class ProjController extends BaseController {
 	 * @throws Exception
 	 */
 	@RequestMapping(value = { "/codeGen/genCode/" }, method = RequestMethod.GET)
-	public String genCodeUI(HttpSession session, @RequestParam(required = true) String lang_id,
-			@RequestParam(required = true) String db_id, @RequestParam(required = true) String proj_id,
-			@RequestParam(required = true) String entity_id, Map<String, Object> map) throws Exception {
+	public String genCodeUI(HttpSession session,
+			@RequestParam(required = true) String lang_id,
+			@RequestParam(required = true) String db_id,
+			@RequestParam(required = true) String proj_id,
+			@RequestParam(required = true) String entity_id,
+			Map<String, Object> map) throws Exception {
 
 		Proj p = projService.getById(proj_id);
 
@@ -72,7 +80,8 @@ public class ProjController extends BaseController {
 		pep.setProj_id(proj_id);
 		pep.setEntity_id(pe.getId());
 
-		List<ProjEntityProp> list_pep = projEntityPropService.findByProjEntityProp(pep, 1, Integer.MAX_VALUE);
+		List<ProjEntityProp> list_pep = projEntityPropService
+				.findByProjEntityProp(pep, 1, Integer.MAX_VALUE);
 
 		Map<String, Object> model = new HashMap<String, Object>();
 		model.put("data_p", p);
@@ -86,16 +95,20 @@ public class ProjController extends BaseController {
 
 		map.put("random", "");
 
-		map.put("temp_" + lang_id + "_model", Processor.getResult("model_" + lang_id, model));
-		map.put("temp_" + lang_id + "_biz", Processor.getResult("biz_" + lang_id, model));
-		map.put("temp_" + db_id, TempUtil.genSQLCreateTable(db_id, pe, list_pep));
-		map.put("temp_" + lang_id + "_mapper", Processor.getResult("mapper_xml", model));
+		map.put("temp_" + lang_id + "_model",
+				Processor.getResult("model_" + lang_id, model));
+		map.put("temp_" + lang_id + "_biz",
+				Processor.getResult("biz_" + lang_id, model));
+		map.put("temp_" + db_id,
+				TempUtil.genSQLCreateTable(db_id, pe, list_pep));
+		map.put("temp_" + lang_id + "_mapper",
+				Processor.getResult("mapper_xml", model));
 
 		return "codeGen/project/genCode";
 	}
 
 	/**
-	 * 
+	 *
 	 * @param session
 	 * @param page
 	 * @param rows
@@ -103,20 +116,23 @@ public class ProjController extends BaseController {
 	 * @return
 	 */
 	@RequestMapping(value = { "/codeGen/proj_create/" }, method = RequestMethod.GET)
-	public String createUI(HttpSession session, @RequestParam(required = false, defaultValue = "1") int page,
-			@RequestParam(required = false, defaultValue = "100") int rows, Map<String, Object> map) {
+	public String createUI(HttpSession session,
+			@RequestParam(required = false, defaultValue = "1") int page,
+			@RequestParam(required = false, defaultValue = "100") int rows,
+			Map<String, Object> map) {
 
 		map.put("session_user", session.getAttribute("session.user"));
 		map.put("nav_choose", ",05,0501,");
 
-		List<Proj> proj_list = projService.findByProj(null, page, Integer.MAX_VALUE);
+		List<Proj> proj_list = projService.findByProj(null, page,
+				Integer.MAX_VALUE);
 		map.put("data_proj_list", proj_list);
 
 		return "codeGen/project/index";
 	}
 
 	/**
-	 * 
+	 *
 	 * @param session
 	 * @param map
 	 * @param id
@@ -135,15 +151,15 @@ public class ProjController extends BaseController {
 	}
 
 	/**
-	 * 
+	 *
 	 * @param session
 	 * @param map
 	 * @param id
 	 * @return
 	 */
 	@RequestMapping(value = { "/codeGen/templet_choose_codeGen/" }, method = RequestMethod.GET)
-	public String templet_choose_codeGenUI(HttpSession session, Map<String, Object> map,
-			@RequestParam(required = false) String id) {
+	public String templet_choose_codeGenUI(HttpSession session,
+			Map<String, Object> map, @RequestParam(required = false) String id) {
 
 		id = StringUtil.isEmpty(id);
 
@@ -161,8 +177,12 @@ public class ProjController extends BaseController {
 		ProjEntity pe = new ProjEntity();
 		pe.setProj_id(id);
 
-		List<ProjEntity> list_pe = projEntityService.findByProjEntity(pe, 1, Integer.MAX_VALUE);
+		List<ProjEntity> list_pe = projEntityService.findByProjEntity(pe, 1,
+				Integer.MAX_VALUE);
 		map.put("data_list_pe", list_pe);
+
+		List<ProjForm> list = projFormService.findByProjId(id);
+		map.put("data_list_pf", list);
 
 		return "codeGen/project/templet_choose_codeGen";
 	}
