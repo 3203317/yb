ca268f2c896e6a483cba944bb0071f370a11be54
3203317 <3203317@qq.com>
2018-05-08 09:26:16 +0800

    haha

--- abc-app-xxx/assets/template/mysql.sql
+++ abc-app-xxx/assets/template/mysql.sql
@@ -8,6 +8,6 @@ create table ${(data_pe.db_tab_name)!}
   primary key (<#list data_list_pep_db_pk! as doc><#if 1==doc.is_pk>`${(doc.id)!}`<#if doc_has_next>, </#if></#if></#list>),
 </#if>
   <#import "data_type_mysql" as data_type>
-<#list data_list_pep_db! as doc>  `${(doc.id)!}` <@data_type.m name="${(doc.prop_type)!}" len="${(doc.len_max)?c}"/><#if 1!=doc.is_null> not null</#if> comment '${(doc.prop_desc)!}'<#if doc_has_next>,</#if>
+<#list data_list_pep_db! as doc>  `${(doc.id)!}` <@data_type.m name="${(doc.prop_type)!}" len="${(doc.len_max)?c}"/><#if 1!=doc.allow_null> not null</#if> comment '${(doc.prop_desc)!}'<#if doc_has_next>,</#if>
 </#list>
 );
